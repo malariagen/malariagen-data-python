f095b8a7917c8f3972ffc47997c82ad2ebf3ca7c
Alistair Miles <alimanfoo@googlemail.com>
2021-01-07 23:37:30 +0000

    add tests

--- tests/test_ag3.py
+++ tests/test_ag3.py
@@ -1,6 +1,8 @@
 from malariagen_data import Ag3
 import pandas
 from pandas.testing import assert_frame_equal
+import dask.array as da
+import numpy as np
 
 
 gcs_url = "gs://vo_agam_release/"
@@ -114,3 +116,94 @@ def test_sample_metadata():
     assert expected_cols + pca_cols == tuple(df_samples_pca.columns)
     assert len(df_samples_v3_wild) == len(df_samples_pca)
     assert expected_species == set(df_samples_pca["species"])
+
+
+def test_site_filters():
+
+    ag3 = Ag3(gcs_url)
+    for mask in "gamb_colu_arab", "gamb_colu", "arab":
+        for seq_id in "2R", "2L", "3R", "3L", "X":
+            filter_pass = ag3.site_filters(seq_id=seq_id, mask=mask)
+            assert isinstance(filter_pass, da.Array)
+            assert 1 == filter_pass.ndim
+            assert bool == filter_pass.dtype
+
+
+def test_snp_sites():
+
+    ag3 = Ag3(gcs_url)
+    for seq_id in "2R", "2L", "3R", "3L", "X":
+        pos, ref, alt = ag3.snp_sites(seq_id=seq_id)
+        assert isinstance(pos, da.Array)
+        assert 1 == pos.ndim
+        assert "i4" == pos.dtype
+        assert isinstance(ref, da.Array)
+        assert 1 == ref.ndim
+        assert "S1" == ref.dtype
+        assert isinstance(alt, da.Array)
+        assert 2 == alt.ndim
+        assert "S1" == alt.dtype
+        assert pos.shape[0] == ref.shape[0] == alt.shape[0]
+
+    # specific field
+    pos = ag3.snp_sites(seq_id="3R", field="POS")
+    assert isinstance(pos, da.Array)
+    assert 1 == pos.ndim
+    assert "i4" == pos.dtype
+
+    # apply site mask
+    filter_pass = ag3.site_filters(seq_id="X", mask="gamb_colu_arab").compute()
+    pos_pass = ag3.snp_sites(seq_id="X", field="POS", site_mask="gamb_colu_arab")
+    assert isinstance(pos_pass, da.Array)
+    assert 1 == pos_pass.ndim
+    assert "i4" == pos_pass.dtype
+    assert np.count_nonzero(filter_pass) == pos_pass.shape[0]
+
+
+def test_snp_genotypes():
+
+    cohorts = (
+        "v3",
+        "v3_wild",
+        "AG1000G-X",
+        ["AG1000G-BF-A", "AG1000G-BF-B", "AG1000G-BF-C"],
+    )
+
+    ag3 = Ag3(gcs_url)
+    for cohort in cohorts:
+        df_samples = ag3.sample_metadata(cohort=cohort, species_calls=None)
+        for seq_id in "2R", "2L", "3R", "3L", "X":
+            gt = ag3.snp_genotypes(seq_id=seq_id, cohort=cohort)
+            assert isinstance(gt, da.Array)
+            assert 3 == gt.ndim
+            assert "i1" == gt.dtype
+            assert len(df_samples) == gt.shape[1]
+
+    # specific fields
+    x = ag3.snp_genotypes(seq_id="X", field="GT")
+    assert isinstance(x, da.Array)
+    assert 3 == x.ndim
+    assert "i1" == x.dtype
+    x = ag3.snp_genotypes(seq_id="X", field="GQ")
+    assert isinstance(x, da.Array)
+    assert 2 == x.ndim
+    assert "i2" == x.dtype
+    x = ag3.snp_genotypes(seq_id="X", field="MQ")
+    assert isinstance(x, da.Array)
+    assert 2 == x.ndim
+    assert "i2" == x.dtype
+    x = ag3.snp_genotypes(seq_id="X", field="AD")
+    assert isinstance(x, da.Array)
+    assert 3 == x.ndim
+    assert "i2" == x.dtype
+
+    # site mask
+    filter_pass = ag3.site_filters(seq_id="X", mask="gamb_colu_arab").compute()
+    df_samples = ag3.sample_metadata()
+    gt_pass = ag3.snp_genotypes(seq_id="X", site_mask="gamb_colu_arab")
+    assert isinstance(gt_pass, da.Array)
+    assert 3 == gt_pass.ndim
+    assert "i1" == gt_pass.dtype
+    assert np.count_nonzero(filter_pass) == gt_pass.shape[0]
+    assert len(df_samples) == gt_pass.shape[1]
+    assert 2 == gt_pass.shape[2]
