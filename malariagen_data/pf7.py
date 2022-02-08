02ea8f6d54b75c99ab4638d681ca36ffe88765f4
kathryn1995 <km22@sanger.ac.uk>
2022-02-08 15:07:49 +0000

    Extend documentation

--- malariagen_data/pf7.py
+++ malariagen_data/pf7.py
@@ -22,6 +22,30 @@ DIM_GENOTYPES = "genotypes"
 
 
 class Pf7:
+    """Provides access to data from the Pf7 release.
+
+    Parameters
+    ----------
+        url : str
+            Base path to data. Give "gs://gs://pf7_release/" to use Google Cloud Storage,
+            or a local path on your file system if data have been downloaded.
+        data_config (optional) : path to config for structure of Pf7 data resource. Defaults to config in repository.
+        **kwargs
+            Passed through to fsspec when setting up file system access.
+
+    Examples
+    --------
+    Access data from Google Cloud Storage (default):
+
+        >>> import malariagen_data
+        >>> pf7 = malariagen_data.Pf7()
+
+    Access data downloaded to a local file system:
+
+        >>> pf7 = malariagen_data.Pf7("/local/path/to/pf7_release/")
+
+    """
+
     def __init__(
         self,
         url,
@@ -128,6 +152,7 @@ class Pf7:
         }
 
     def _load_config(self, data_config):
+        """Load the config supplied into json format."""
         if not data_config:
             working_dir = os.path.dirname(os.path.abspath(__file__))
             data_config = os.path.join(working_dir, "pf7_config.json")
@@ -136,10 +161,10 @@ class Pf7:
         return config_content
 
     def sample_metadata(self):
-        """Access sample metadata.
+        """Access sample metadata and return as pandas dataframe.
         Returns
         -------
-        df : pandas.DataFrame
+            df : pandas.DataFrame
         """
         if self._cache_sample_metadata is None:
             path = os.path.join(self._path, self.CONF["metadata_path"])
@@ -148,6 +173,12 @@ class Pf7:
         return self._cache_sample_metadata
 
     def open_variant_calls_zarr(self):
+        """Open variant calls zarr
+
+        Returns
+        -------
+            root : zarr.hierarchy.Group
+        """
         if self._cache_variant_calls_zarr is None:
             path = os.path.join(self._path, self.CONF["variant_calls_zarr_path"])
             store = init_zarr_store(fs=self._fs, path=path)
@@ -155,6 +186,7 @@ class Pf7:
         return self._cache_variant_calls_zarr
 
     def _add_coordinates(self, root, inline_array, chunks, var_names_for_outputs):
+        """Add coordinate variables in zarr to dictionary"""
         # coordinates
         coords = dict()
         for var_name in "POS", "CHROM":
@@ -168,6 +200,7 @@ class Pf7:
         return coords
 
     def _add_data_vars(self, root, inline_array, chunks, var_names_for_outputs):
+        """Add default set of variables from zarr to dictionary"""
         data_vars = dict()
 
         # variant_allele
@@ -200,6 +233,7 @@ class Pf7:
         return data_vars
 
     def _add_extended_data(self, root, inline_array, chunks, data_vars):
+        """Add all variables not included in default set to dictionary"""
 
         subset_extended_variants = self.extended_variant_fields
         subset_extended_calldata = self.extended_calldata_variables
@@ -223,16 +257,18 @@ class Pf7:
 
     def variant_calls(self, extended=False, inline_array=True, chunks="native"):
         """Access variant sites, site filters and genotype calls.
+
         Parameters
         ----------
-        inline_array : bool, optional
-            Passed through to dask.array.from_array().
-        chunks : str, optional
-            If 'auto' let dask decide chunk size. If 'native' use native zarr chunks.
-            Also can be a target size, e.g., '200 MiB'.
+            extended (bool, optional): If False only the default variables are returned. If True all variables from the zarr are returned.
+                                        Defaults to False.
+            inline_array (bool, optional): Passed through to dask.array.from_array(). Defaults to True.
+            chunks (str, optional): If 'auto' let dask decide chunk size. If 'native' use native zarr chunks.
+                                    Also can be a target size, e.g., '200 MiB'. Defaults to "native".
+
         Returns
         -------
-        ds : xarray.Dataset
+            ds (xarray.Dataset): Dataset containing either defualt or extended variables from the variant calls Zarr.
         """
 
         # setup
