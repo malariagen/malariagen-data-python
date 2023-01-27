ab4d5c15046b395cc8805769442aada130947d63
Alistair Miles <alimanfoo@googlemail.com>
2023-01-27 14:04:45 +0000

    remove redundant comment

--- malariagen_data/anopheles.py
+++ malariagen_data/anopheles.py
@@ -2458,8 +2458,6 @@ class AnophelesDataResource(ABC):
 
         import igv_notebook
 
-        # TODO deal with funestus gene annotations
-
         config = {
             "reference": {
                 "id": self._genome_ref_id,
