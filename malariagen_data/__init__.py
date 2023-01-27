a7595aa6783e22e78d2107588aba5d326d27dbae
Alistair Miles <alimanfoo@googlemail.com>
2023-01-27 15:33:19 +0000

    ensure xarray has zarr backend

--- malariagen_data/__init__.py
+++ malariagen_data/__init__.py
@@ -12,6 +12,23 @@ try:
 except ModuleNotFoundError:
     import importlib_metadata
 
-
 # this will read version from pyproject.toml
 __version__ = importlib_metadata.version(__name__)
+
+
+def _ensure_xarray_zarr_backend():
+    # workaround for https://github.com/malariagen/malariagen-data-python/issues/320
+    # see also https://github.com/pydata/xarray/issues/7478
+
+    from xarray.backends.plugins import list_engines
+    from xarray.backends.zarr import ZarrBackendEntrypoint
+
+    # zarr is a dependency, so will always be available if malariagen_data
+    # is installed
+    ZarrBackendEntrypoint.available = True
+
+    # ensure xarray refreshes list of available engines
+    list_engines.cache_clear()
+
+
+_ensure_xarray_zarr_backend()
