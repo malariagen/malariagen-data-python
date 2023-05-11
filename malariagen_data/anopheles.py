61bb489f61b31606746834f9726b0fc5eeced0f5
Alistair Miles <alimanfoo@googlemail.com>
2023-05-11 20:50:21 +0100

    Faster xarray concatenation (#395)
    * use faster xarray concatenation
    
    * override attrs
    
    * handle either dask or numpy arrays
    
    * fix typing
    
    * fix import

--- malariagen_data/anopheles.py
+++ malariagen_data/anopheles.py
@@ -47,8 +47,8 @@ from .util import (
     locate_region,
     plotly_discrete_legend,
     resolve_region,
+    simple_xarray_concat,
     type_error,
-    xarray_concat,
 )
 
 AA_CHANGE_QUERY = (
@@ -1956,7 +1956,7 @@ class AnophelesDataResource(
                 ly.append(y)
 
             debug("concatenate data from multiple sample sets")
-            x = xarray_concat(ly, dim=DIM_SAMPLE)
+            x = simple_xarray_concat(ly, dim=DIM_SAMPLE)
 
             debug("add variants variables")
             v = self._snp_variants_for_contig(
@@ -1982,7 +1982,7 @@ class AnophelesDataResource(
             lx.append(x)
 
         debug("concatenate data from multiple regions")
-        ds = xarray_concat(lx, dim=DIM_VARIANT)
+        ds = simple_xarray_concat(lx, dim=DIM_VARIANT)
 
         if site_mask is not None:
             debug("apply site filters")
@@ -2114,7 +2114,7 @@ class AnophelesDataResource(
             lx.append(x)
 
         debug("concatenate data from multiple regions")
-        ds = xarray_concat(lx, dim=DIM_VARIANT)
+        ds = simple_xarray_concat(lx, dim=DIM_VARIANT)
 
         debug("apply site filters")
         if site_mask is not None:
@@ -5285,7 +5285,7 @@ class AnophelesDataResource(
                 return None
 
             debug("concatenate data from multiple sample sets")
-            x = xarray_concat(ly, dim=DIM_SAMPLE)
+            x = simple_xarray_concat(ly, dim=DIM_SAMPLE)
 
             debug("handle region")
             if r.start or r.end:
@@ -5296,7 +5296,7 @@ class AnophelesDataResource(
             lx.append(x)
 
         debug("concatenate data from multiple regions")
-        ds = xarray_concat(lx, dim=DIM_VARIANT)
+        ds = simple_xarray_concat(lx, dim=DIM_VARIANT)
 
         debug("handle sample query")
         if sample_query is not None:
