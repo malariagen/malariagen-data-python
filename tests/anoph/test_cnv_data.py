79b93c83746a1ef3ae3fb9c699896c0e6b4f689f
_ <4256466+leehart@users.noreply.github.com>
2023-07-06 12:00:01 +0100

    Add tests for AnophelesCnvData cnv_discordant_read_calls() w/ bad params. Use random good params.

--- tests/anoph/test_cnv_data.py
+++ tests/anoph/test_cnv_data.py
@@ -129,13 +129,15 @@ def test_open_cnv_coverage_calls(fixture, api: AnophelesCnvData):
     # Check with a sample set that should not exist
     with pytest.raises(ValueError):
         root = api.open_cnv_coverage_calls(
-            sample_set="foobar", analysis=api.coverage_calls_analysis_ids[0]
+            sample_set="foobar", analysis=random.choice(api.coverage_calls_analysis_ids)
         )
 
     # Check with an analysis that should not exist
-    sample_set = next(api.sample_sets().itertuples()).sample_set
+    all_sample_sets = api.sample_sets()["sample_set"].to_list()
     with pytest.raises(ValueError):
-        root = api.open_cnv_coverage_calls(sample_set=sample_set, analysis="foobar")
+        root = api.open_cnv_coverage_calls(
+            sample_set=random.choice(all_sample_sets), analysis="foobar"
+        )
 
     # Check with a sample set and analysis that should not exist
     with pytest.raises(ValueError):
@@ -530,3 +532,19 @@ def test_cnv_discordant_read_calls(fixture, api: AnophelesCnvData):
             assert isinstance(d1, xr.DataArray)
             d2 = ds["call_genotype"].sum(axis=1)
             assert isinstance(d2, xr.DataArray)
+
+    # Check with a contig that should not exist
+    with pytest.raises(ValueError):
+        api.cnv_discordant_read_calls(
+            contig="foobar", sample_sets=random.choice(all_sample_sets)
+        )
+
+    # Check with a sample set that should not exist
+    with pytest.raises(ValueError):
+        api.cnv_discordant_read_calls(
+            contig=random.choice(api.contigs), sample_sets="foobar"
+        )
+
+    # Check with a contig and sample set that should not exist
+    with pytest.raises(ValueError):
+        api.cnv_discordant_read_calls(contig="foobar", sample_sets="bazqux")
