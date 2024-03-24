4275a288b1be69c20d843d0da1719f761f92689e
Alistair Miles <alimanfoo@googlemail.com>
2024-03-24 23:41:47 +0000

    fix typing

--- malariagen_data/anoph/cnv_data.py
+++ malariagen_data/anoph/cnv_data.py
@@ -784,10 +784,10 @@ class AnophelesCnvData(
         debug("set up plot title")
         title = "CNV HMM"
         if sample_sets is not None:
-            if isinstance(sample_sets, (list, tuple)):
-                sample_sets_text = ", ".join(sample_sets)
-            else:
+            if isinstance(sample_sets, str):
                 sample_sets_text = sample_sets
+            else:
+                sample_sets_text = ", ".join(sample_sets)
             title += f" - {sample_sets_text}"
         if sample_query is not None:
             title += f" ({sample_query})"
