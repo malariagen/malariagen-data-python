6d4a24790cd3d27ee1f1a58c13ae777f5a865563
jonbrenas <51911846+jonbrenas@users.noreply.github.com>
2024-06-21 10:42:21 +0200

    Ruffed up

--- malariagen_data/anoph/genome_features.py
+++ malariagen_data/anoph/genome_features.py
@@ -361,8 +361,9 @@ class AnophelesGenomeFeaturesData(AnophelesGenomeSequenceData):
         data["bottom"] = np.where(data["strand"] == "+", 1, 0)
         data["top"] = data["bottom"] + 0.8
 
-        # debug("tidy up missing values for presentation")
-        # data.fillna("", inplace=True)
+        debug("tidy up missing values for presentation")
+        data = data.drop(["score", "phase"], axis=1)
+        data.fillna("", inplace=True)
 
         debug("make a figure")
         xwheel_zoom = bokeh.models.WheelZoomTool(
