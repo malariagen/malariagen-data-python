fix and improve plotting pairwise average fst (#579)

malariagen_data/anoph/fst.py
@@ -485,32 +485,49 @@ class AnophelesFstAnalysis(
         zmax: Optional[plotly_params.zmax] = None,
         text_auto: plotly_params.text_auto = ".3f",
         color_continuous_scale: plotly_params.color_continuous_scale = "gray_r",
-        width: plotly_params.fig_width = 700,
-        height: plotly_params.fig_height = 600,
+        width: plotly_params.fig_width = None,
+        height: plotly_params.fig_height = None,
+        row_height: plotly_params.height = 40,
+        col_width: plotly_params.width = 50,
         show: plotly_params.show = True,
         renderer: plotly_params.renderer = None,
         **kwargs,
     ):
-        # setup df
-        cohort_list = np.unique(fst_df[["cohort1", "cohort2"]].values)
-        # df to fill
-        fig_df = pd.DataFrame(columns=cohort_list, index=cohort_list)
-        # fill df from fst_df
-        for index_key in range(len(fst_df)):
-            index = fst_df.iloc[index_key]["cohort1"]
-            col = fst_df.iloc[index_key]["cohort2"]
-            fst = fst_df.iloc[index_key]["fst"]
-            fig_df[index][col] = fst
-            if annotation == "standard error":
-                se = fst_df.iloc[index_key]["se"]
-                fig_df[col][index] = se
-            elif annotation == "Z score":
-                zs = fst_df.iloc[index_key]["fst"] / fst_df.iloc[index_key]["se"]
-                fig_df[col][index] = zs
-            else:
-                fig_df.loc[index, col] = fst
+        # Obtain a list of all cohorts analysed. N.B., preserve the order in
+        # which the cohorts are provided in the input dataframe.
+        cohorts = pd.unique(fst_df[["cohort1", "cohort2"]].values.flatten())
 
-        # create plot
+        # Create a dataframe to visualise as a heatmap.
+        fig_df = pd.DataFrame(columns=cohorts, index=cohorts)
+
+        # Set up plot title.
+        title = "<i>F</i><sub>ST</sub>"
+        if annotation is not None:
+            title += " ⧅ " + annotation
+
+        # Fill the figure dataframe from the Fst dataframe.
+        for cohort1, cohort2, fst, se in fst_df.itertuples(index=False):
+            fig_df.loc[cohort2, cohort1] = fst
+            if annotation == "standard error":
+                fig_df.loc[cohort1, cohort2] = se
+            elif annotation == "Z score":
+                zs = fst / se
+                fig_df.loc[cohort1, cohort2] = zs
+            else:
+                fig_df.loc[cohort1, cohort2] = fst
+
+        # Don't colour the plot if the upper triangle is SE or Z score,
+        # as the colouring doesn't really make sense.
+        if annotation is not None and zmax is None:
+            zmax = 1e9
+
+        # Dynamically size the figure based on number of cohorts.
+        if height is None:
+            height = 100 + len(cohorts) * row_height
+        if width is None:
+            width = 200 + len(cohorts) * col_width
+
+        # Create plot.
         with np.errstate(invalid="ignore"):
             fig = px.imshow(
                 img=fig_df,
@@ -523,7 +540,11 @@ class AnophelesFstAnalysis(
                 aspect="auto",
                 **kwargs,
             )
-        fig.update_layout(plot_bgcolor="rgba(0,0,0,0)")
+        fig.update_layout(
+            plot_bgcolor="rgba(0,0,0,0)",
+            coloraxis_showscale=False,
+            title=title,
+        )
         fig.update_yaxes(showgrid=False, linecolor="black")
         fig.update_xaxes(showgrid=False, linecolor="black")
 