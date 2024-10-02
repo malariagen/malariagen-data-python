01ad5e34160c778eb1d454eb199ca883479b80db
jonbrenas <51911846+jonbrenas@users.noreply.github.com>
2024-10-02 09:52:30 +0100

    Updated the tests a bit.

--- tests/anoph/test_h12.py
+++ tests/anoph/test_h12.py
@@ -142,10 +142,22 @@ def check_h12_gwss(*, api, h12_params):
     assert np.all(h12 >= 0)
     assert np.all(h12 <= 1)
 
+    circle_kwargs_dict = {
+        0: {"line_color": "black", "size": 5, "line_width": 1, "fill_color": None},
+        1: {"line_color": "green", "size": 4, "line_width": 2, "fill_color": "black"},
+        2: {"line_color": "orange", "size": 3, "line_width": 1, "fill_color": None},
+        3: {"line_color": "green", "size": 2, "line_width": 2, "fill_color": "black"},
+        4: {"line_color": "purple", "size": 1, "line_width": 1, "fill_color": None},
+    }
+
     # Check plotting functions.
-    fig = api.plot_h12_gwss_track(**h12_params, show=False)
+    fig = api.plot_h12_gwss_track(
+        **h12_params, circle_kwargs_dict=circle_kwargs_dict, show=False
+    )
     assert isinstance(fig, bokeh.models.Plot)
-    fig = api.plot_h12_gwss(**h12_params, show=False)
+    fig = api.plot_h12_gwss(
+        **h12_params, circle_kwargs_dict=circle_kwargs_dict, show=False
+    )
     assert isinstance(fig, bokeh.models.GridPlot)
 
 
