c273c1d8b0d140f478bd046a602d9bafdd0c1e87
Jon Brenas <51911846+jonbrenas@users.noreply.github.com>
2024-10-17 14:05:09 +0100

    Update malariagen_data/anoph/h12.py
    Co-authored-by: Alistair Miles <alimanfoo@googlemail.com>

--- malariagen_data/anoph/h12.py
+++ malariagen_data/anoph/h12.py
@@ -516,7 +516,7 @@ class AnophelesH12Analysis(
 
     @check_types
     @doc(
-        summary="Plot h12 GWSS data track with multiple traces.",
+        summary="Plot h12 GWSS data track with multiple traces overlaid.",
     )
     def plot_h12_gwss_multi_overlay_track(
         self,
