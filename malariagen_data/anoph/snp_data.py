c84cd1a237062a9033a9ecfcbe1633c455c9ed4d
Tristan Dennis <dennistpw@Tristans-LSTM-MBP.local>
2024-10-18 18:35:47 +1000

    checkout changes from upstream to snpdata.py

--- malariagen_data/anoph/snp_data.py
+++ malariagen_data/anoph/snp_data.py
@@ -1669,23 +1669,9 @@ class AnophelesSnpData(
             coords["variant_position"] = ("variants",), variant_position
 
             # Store alleles, transformed.
-
-            variant_allele = ds_bi["variant_allele"].data
-            variant_allele = variant_allele.rechunk((variant_allele.chunks[0], -1))
-
-            # Chunk allele mapping according to same variant_allele.
-            allele_mapping_chunked = da.from_array(
-                allele_mapping, chunks=variant_allele.chunks
-            )
-
-            # Apply allele mapping blockwise to variant_allele.
-            variant_allele_out = da.map_blocks(
-                lambda allele, map: apply_allele_mapping(allele, map, max_allele=1),
-                variant_allele,
-                allele_mapping_chunked,
-                dtype=variant_allele.dtype,
-                chunks=(variant_allele.chunks[0], [2]),
-
+            variant_allele_dask = ds_bi["variant_allele"].data
+            variant_allele_out = dask_apply_allele_mapping(
+                variant_allele_dask, allele_mapping, max_allele=1
             )
             data_vars["variant_allele"] = ("variants", "alleles"), variant_allele_out
 
