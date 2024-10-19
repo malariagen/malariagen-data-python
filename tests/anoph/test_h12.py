4fb0ba108379342807cf922b88cdf5b505f2bcfa
jonbrenas <51911846+jonbrenas@users.noreply.github.com>
2024-10-19 16:30:29 +0100

    Added a test for the dict version of window_size

--- tests/anoph/test_h12.py
+++ tests/anoph/test_h12.py
@@ -241,6 +241,29 @@ def test_h12_gwss_multi_with_default_analysis(fixture, api: AnophelesH12Analysis
 
 
 @parametrize_with_cases("fixture,api", cases=".")
+def test_h12_gwss_multi_with_window_size_dict(fixture, api: AnophelesH12Analysis):
+    # Set up test parameters.
+    all_sample_sets = api.sample_sets()["sample_set"].to_list()
+    all_countries = api.sample_metadata()["country"].unique().tolist()
+    country1, country2 = random.sample(all_countries, 2)
+    cohort1_query = f"country == '{country1}'"
+    cohort2_query = f"country == '{country2}'"
+    h12_params = dict(
+        contig=random.choice(api.contigs),
+        sample_sets=all_sample_sets,
+        window_size={
+            "cohort1": random.randint(100, 500),
+            "cohort2": random.randint(100, 500),
+        },
+        min_cohort_size=1,
+        cohorts={"cohort1": cohort1_query, "cohort2": cohort2_query},
+    )
+
+    # Run checks.
+    check_h12_gwss_multi(api=api, h12_params=h12_params)
+
+
+@parametrize_with_cases("fixture,api", cases=".")
 def test_h12_gwss_multi_with_analysis(fixture, api: AnophelesH12Analysis):
     # Set up test parameters.
     all_sample_sets = api.sample_sets()["sample_set"].to_list()
