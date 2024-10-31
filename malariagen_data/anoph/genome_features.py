4658378889625af56d570126864c3d9cb4e2fcc2
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2024-10-31 11:36:45 +0000

    fix reset index after

--- malariagen_data/anoph/genome_features.py
+++ malariagen_data/anoph/genome_features.py
@@ -149,12 +149,12 @@ class AnophelesGenomeFeaturesData(AnophelesGenomeSequenceData):
                         df_part = df_part.query(f"end >= {r.start}")
                     parts.append(df_part)
                 df = pd.concat(parts, axis=0)
-                return df.reset_index(drop=True).sort_values(["contig", "start"]).copy()
+                return df.sort_values(["contig", "start"]).reset_index(drop=True).copy()
 
             return (
                 self._genome_features(attributes=attributes_normed)
-                .reset_index(drop=True)
                 .sort_values(["contig", "start"])
+                .reset_index(drop=True)
                 .copy()
             )
 
