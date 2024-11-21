5abc24054e442c98fccc6c0f06d58af946942f7f
jonbrenas <51911846+jonbrenas@users.noreply.github.com>
2024-11-21 17:02:57 +0000

    Solved the MRO issue

--- malariagen_data/anopheles.py
+++ malariagen_data/anopheles.py
@@ -40,7 +40,7 @@ from .anoph.base import AnophelesBase
 from .anoph.cnv_data import AnophelesCnvData
 from .anoph.genome_features import AnophelesGenomeFeaturesData
 from .anoph.genome_sequence import AnophelesGenomeSequenceData
-from .anoph.hap_data import hap_params
+from .anoph.hap_data import AnophelesHapData, hap_params
 from .anoph.hap_frq import AnophelesHapFrequencyAnalysis
 from .anoph.igv import AnophelesIgv
 from .anoph.pca import AnophelesPca
@@ -104,9 +104,9 @@ class AnophelesDataResource(
     AnophelesPca,
     AnophelesIgv,
     AnophelesAimData,
+    AnophelesHapData,
     AnophelesSnpData,
     AnophelesCnvData,
-    # AnophelesHapData,
     AnophelesSampleMetadata,
     AnophelesGenomeFeaturesData,
     AnophelesGenomeSequenceData,
