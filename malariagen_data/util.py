773d00e32434ac71ac70d2a317b1bacd15de0811
Alistair Miles <alimanfoo@googlemail.com>
2024-12-02 18:03:53 +0000

    make handling of chained URLs consistent between GCS and S3

--- malariagen_data/util.py
+++ malariagen_data/util.py
@@ -450,15 +450,18 @@ def init_filesystem(url, **kwargs):
                 scopes=["https://www.googleapis.com/auth/cloud-platform"]
             )
 
-        # Ensure credentials are passed through to gcsfs.
+        kwargs.setdefault("token", credentials)
+
+        # Ensure options are passed through to gcsfs, even if URL is chained.
         if url.startswith("gs://") or url.startswith("gcs://"):
-            kwargs["token"] = credentials
+            storage_options = kwargs
         elif "gs://" in url:
             # Chained URL.
-            kwargs["gs"] = dict(token=credentials)
+            storage_options = {"gs": kwargs}
         elif "gcs://" in url:
             # Chained URL.
-            kwargs["gcs"] = dict(token=credentials)
+            storage_options = {"gcs": kwargs}
+
     elif "s3://" in url:
         # N.B., we currently assume any S3 URLs refer to buckets hosted at Sanger.
         config = {
@@ -471,11 +474,14 @@ def init_filesystem(url, **kwargs):
         kwargs.setdefault("endpoint_url", "https://cog.sanger.ac.uk")
         kwargs.setdefault("config_kwargs", config)
 
-        if not url.startswith("s3://"):
+        if url.startswith("s3://"):
+            storage_options = kwargs
+        else:
             # Chained URL.
-            kwargs.setdefault("cache_storage", "gcs_cache")
+            storage_options = {"s3": kwargs}
+
     # Process the URL using fsspec.
-    fs, path = url_to_fs(url, **kwargs)
+    fs, path = url_to_fs(url, **storage_options)
 
     # Path compatibility, fsspec/gcsfs behaviour varies between versions.
     while path.endswith("/"):
