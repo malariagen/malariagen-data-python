a2c427532b4c36ae6cc7eac7bfda573b09aad3d3
jonbrenas <51911846+jonbrenas@users.noreply.github.com>
2024-12-13 10:45:48 +0000

    Testing something

--- tests/anoph/test_dipclust.py
+++ tests/anoph/test_dipclust.py
@@ -128,6 +128,7 @@ def test_plot_diplotype_clustering_advanced(
     dipclust_params = dict(
         region=fixture.random_region_str(region_size=5000),
         sample_sets=[random.choice(all_sample_sets)],
+        heterozygosity=False,
         linkage_method=random.choice(linkage_methods),
         distance_metric=distance_metric,
         sample_query=random.choice(sample_queries),
@@ -160,6 +161,7 @@ def test_plot_diplotype_clustering_advanced_with_transcript(
     dipclust_params = dict(
         region=contig,
         snp_transcripts=transcripts,
+        heterozygosity=False,
         sample_sets=[random.choice(all_sample_sets)],
         linkage_method=random.choice(linkage_methods),
         distance_metric="cityblock",
@@ -191,6 +193,7 @@ def test_plot_diplotype_clustering_advanced_with_cnv_region(
     dipclust_params = dict(
         region=region,
         cnv_region=region,
+        heterozygosity=False,
         sample_sets=[random.choice(all_sample_sets)],
         linkage_method=random.choice(linkage_methods),
         distance_metric="cityblock",
