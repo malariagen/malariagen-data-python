e5b55dc0ac814d1083fe4e7a77bc0a2cd3ec36d8
KellyLBennett <kellylb13@yahoo.com>
2025-05-02 12:58:35 +0100

    clean up PR

--- malariagen_data/anoph/admixture_stats.py
+++ malariagen_data/anoph/admixture_stats.py
@@ -151,3 +151,123 @@ class AnophelesAdmixtureAnalysis(
         )
 
         return f3, se, z
+
+    def patterson_f4(
+        self,
+        a_query: base_params.sample_query,
+        b_query: base_params.sample_query,
+        c_query: base_params.sample_query,
+        d_query: base_params.sample_query,
+        region: base_params.region,
+        site_mask: Optional[base_params.site_mask] = base_params.DEFAULT,
+        n_jack: base_params.n_jack = 200,
+        max_missing_an: base_params.max_missing_an = 0,
+        segregating_mode: admixture_stats_params.segregating_mode = "recipient",
+        cohort_size: Optional[
+            base_params.cohort_size
+        ] = admixture_stats_params.cohort_size_default,
+        min_cohort_size: Optional[
+            base_params.min_cohort_size
+        ] = admixture_stats_params.min_cohort_size_default,
+        max_cohort_size: Optional[
+            base_params.max_cohort_size
+        ] = admixture_stats_params.max_cohort_size_default,
+    ) -> Tuple[float, float, float]:
+        # Purely for conciseness, internally here we use the scikit-allel convention
+        # of labelling the recipient population "C" and the source populations as
+        # "A" and "B".
+
+        # Compute allele counts for the three cohorts.
+        aca = self.snp_allele_counts(
+            sample_query=a_query,
+            region=region,
+            site_mask=site_mask,
+            cohort_size=cohort_size,
+            min_cohort_size=min_cohort_size,
+            max_cohort_size=max_cohort_size,
+        )
+
+        acb = self.snp_allele_counts(
+            sample_query=b_query,
+            region=region,
+            site_mask=site_mask,
+            cohort_size=cohort_size,
+            min_cohort_size=min_cohort_size,
+            max_cohort_size=max_cohort_size,
+        )
+
+        acc = self.snp_allele_counts(
+            sample_query=c_query,
+            region=region,
+            site_mask=site_mask,
+            cohort_size=cohort_size,
+            min_cohort_size=min_cohort_size,
+            max_cohort_size=max_cohort_size,
+        )
+
+        acd = self.snp_allele_counts(
+            sample_query=d_query,
+            region=region,
+            site_mask=site_mask,
+            cohort_size=cohort_size,
+            min_cohort_size=min_cohort_size,
+            max_cohort_size=max_cohort_size,
+        )
+
+        # Locate biallelic variants and deal with missingness.
+        ac = acc + aca + acb
+        loc_bi = allel.AlleleCountsArray(ac).is_biallelic()
+        ana = np.sum(acc, axis=1)
+        anb = np.sum(aca, axis=1)
+        anc = np.sum(acb, axis=1)
+        an = np.sum(ac, axis=1)  # no. required for sample size
+        n_chroms = an.max()
+        an_missing = n_chroms - an
+        # In addition to applying the max_missing_an threshold, also make sure that
+        # all three cohorts have nonzero allele counts.
+        loc_nomiss = (an_missing <= max_missing_an) & (anc > 0) & (ana > 0) & (anb > 0)
+        loc_sites = loc_bi & loc_nomiss
+        aca_bi = acc[loc_sites]
+        acb_bi = aca[loc_sites]
+        acc_bi = acb[loc_sites]
+        acd_bi = acd[loc_sites]
+        ac_bi = aca_bi + acb_bi + acc_bi + acd_bi
+
+        # Squeeze the allele counts so we end up with only two columns.
+        sqz_mapping = _remap_observed(ac_bi)
+        aca_sqz = allel.AlleleCountsArray(acc_bi).map_alleles(sqz_mapping, max_allele=1)
+        acb_sqz = allel.AlleleCountsArray(aca_bi).map_alleles(sqz_mapping, max_allele=1)
+        acc_sqz = allel.AlleleCountsArray(acb_bi).map_alleles(sqz_mapping, max_allele=1)
+        acd_sqz = allel.AlleleCountsArray(acd_bi).map_alleles(sqz_mapping, max_allele=1)
+
+        # Keep only segregating variants.
+        if segregating_mode == "recipient":
+            loc_seg = acc_sqz.is_segregating()
+        elif segregating_mode == "all":
+            loc_seg = (
+                aca_sqz.is_segregating()
+                & acb_sqz.is_segregating()
+                & acc_sqz.is_segregating()
+            )
+        else:
+            raise ValueError("Invalid value for 'segregating_mode' parameter.")
+        if loc_seg.sum() == 0:
+            raise ValueError("No segregating variants found")
+        else:
+            print(f"Using {loc_seg.sum()} segregating variants for F3 calculation.")
+        aca_seg = acc_sqz[loc_seg]
+        acb_seg = aca_sqz[loc_seg]
+        acc_seg = acb_sqz[loc_seg]
+        acd_seg = acd_sqz[loc_seg]
+
+        # Compute f3 statistic.
+        blen = acc_seg.shape[0] // n_jack
+        f4, se, z, _, _ = allel.average_patterson_d(
+            aca_seg,
+            acb_seg,
+            acc_seg,
+            acd_seg,
+            blen=blen,
+        )
+
+        return f4, se, z
