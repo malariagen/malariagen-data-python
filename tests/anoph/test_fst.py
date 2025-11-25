4989f74f6ab883046ae4dec25b3e83420e3d59fb
tristanpwdennis <tristanpwdennis@gmail.com>
2025-11-25 11:26:18 +1000

    remove tests for g123 and dipclust

--- tests/anoph/test_fst.py
+++ tests/anoph/test_fst.py
@@ -9,7 +9,6 @@ import plotly.graph_objects as go
 
 from malariagen_data import af1 as _af1
 from malariagen_data import ag3 as _ag3
-from malariagen_data import adir1 as _adir1
 from malariagen_data.anoph.fst import AnophelesFstAnalysis
 
 
@@ -55,24 +54,6 @@ def af1_sim_api(af1_sim_fixture):
         default_site_mask="funestus",
         results_cache=af1_sim_fixture.results_cache_path.as_posix(),
         taxon_colors=_af1.TAXON_COLORS,
-    )
-
-
-@pytest.fixture
-def adir1_sim_api(adir1_sim_fixture):
-    return AnophelesFstAnalysis(
-        url=adir1_sim_fixture.url,
-        public_url=adir1_sim_fixture.url,
-        config_path=_adir1.CONFIG_PATH,
-        major_version_number=_adir1.MAJOR_VERSION_NUMBER,
-        major_version_path=_adir1.MAJOR_VERSION_PATH,
-        pre=False,
-        gff_gene_type="protein_coding_gene",
-        gff_gene_name_attribute="Note",
-        gff_default_attributes=("ID", "Parent", "Note", "description"),
-        default_site_mask="dirus",
-        results_cache=adir1_sim_fixture.results_cache_path.as_posix(),
-        taxon_colors=_adir1.TAXON_COLORS,
     )
 
 
