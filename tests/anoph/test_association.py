f72b8c619652cd0c09fb096e49ce64feb18f504d
Sharon Melhi <sharonmelhi365@gmail.com>
2026-03-06 19:07:05 +0530

    test(association): use simulator phenotypes and shared ag3 fixture

--- tests/anoph/test_association.py
+++ tests/anoph/test_association.py
@@ -1,34 +1,28 @@
+import malariagen_data
 import pandas as pd
 import plotly.graph_objects as go  # type: ignore
 import pytest
 import xarray as xr
 
-from malariagen_data.anoph.association import AnophelesAssociationPlotting
+
+@pytest.fixture
+def ag3_sim_api(ag3_sim_fixture):
+    return malariagen_data.Ag3(
+        url=ag3_sim_fixture.url,
+        public_url=ag3_sim_fixture.url,
+        pre=True,
+        check_location=False,
+        show_progress=False,
+        bokeh_output_notebook=False,
+        results_cache=ag3_sim_fixture.results_cache_path.as_posix(),
+    )
 
 
-class DummyAssociationApi(AnophelesAssociationPlotting):
-    def phenotypes_with_snps(self, **kwargs):  # noqa: ARG002
-        return xr.Dataset(
-            {
-                "call_genotype": (
-                    ("variants", "samples", "ploidy"),
-                    [
-                        [[0, 0], [0, 1], [1, 1], [0, 1]],
-                        [[0, 0], [0, 0], [0, 1], [1, 1]],
-                        [[0, 1], [0, 1], [1, 1], [1, 1]],
-                    ],
-                ),
-                "phenotype_binary": (("samples",), [0.0, 0.0, 1.0, 1.0]),
-                "variant_contig": (("variants",), ["2L", "2L", "2R"]),
-                "variant_position": (("variants",), [100, 200, 300]),
-            }
-        )
-
-
-def test_snp_phenotype_association_produces_plot_input():
-    api = DummyAssociationApi()
-
-    df = api.snp_phenotype_association(region="2L:1-1000")
+def test_snp_phenotype_association_produces_plot_input(ag3_sim_api):
+    df = ag3_sim_api.snp_phenotype_association(
+        region="2L",
+        sample_sets=["AG1000G-AO"],
+    )
 
     assert isinstance(df, pd.DataFrame)
     assert {"contig", "position", "pvalue"}.issubset(df.columns)
@@ -36,15 +30,13 @@ def test_snp_phenotype_association_produces_plot_input():
     assert (df["pvalue"].dropna() >= 0).all()
     assert (df["pvalue"].dropna() <= 1).all()
 
-    manhattan = api.plot_manhattan(df, show=False)
-    qq = api.plot_qq(df, show=False)
+    manhattan = ag3_sim_api.plot_manhattan(df, show=False)
+    qq = ag3_sim_api.plot_qq(df, show=False)
     assert isinstance(manhattan, go.Figure)
     assert isinstance(qq, go.Figure)
 
 
-def test_plot_association_from_xarray():
-    api = DummyAssociationApi()
-
+def test_plot_association_from_xarray(ag3_sim_api):
     ds = xr.Dataset(
         {
             "contig": ("variants", ["2L", "2L", "2R", "2R"]),
@@ -53,14 +45,13 @@ def test_plot_association_from_xarray():
         }
     )
 
-    manhattan = api.plot_manhattan(ds, show=False)
-    qq = api.plot_qq(ds, show=False)
+    manhattan = ag3_sim_api.plot_manhattan(ds, show=False)
+    qq = ag3_sim_api.plot_qq(ds, show=False)
     assert isinstance(manhattan, go.Figure)
     assert isinstance(qq, go.Figure)
 
 
-def test_plot_manhattan_missing_column_raises():
-    api = DummyAssociationApi()
+def test_plot_manhattan_missing_column_raises(ag3_sim_api):
     data = pd.DataFrame({"contig": ["2L"], "position": [100]})
     with pytest.raises(ValueError, match="Missing required columns"):
-        api.plot_manhattan(data, show=False)
+        ag3_sim_api.plot_manhattan(data, show=False)
