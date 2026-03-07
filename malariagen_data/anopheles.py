08903d44246409977e304f67b4c8f2e937d3e2f1
Adil <adilraza0206@gmail.com>
2026-03-07 11:58:01 +0530

    Add LD pruning support for biallelic SNP calls

--- malariagen_data/anopheles.py
+++ malariagen_data/anopheles.py
@@ -36,6 +36,7 @@ from .anoph.distance import AnophelesDistanceAnalysis
 from .anoph.sample_metadata import AnophelesSampleMetadata
 from .anoph.snp_data import AnophelesSnpData
 from .anoph.to_plink import PlinkConverter
+from .anoph.ld import AnophelesLdAnalysis
 from .anoph.g123 import AnophelesG123Analysis
 from .anoph.fst import AnophelesFstAnalysis
 from .anoph.h12 import AnophelesH12Analysis
@@ -88,6 +89,7 @@ class AnophelesDataResource(
     AnophelesDistanceAnalysis,
     AnophelesPca,
     PlinkConverter,
+    AnophelesLdAnalysis,
     AnophelesIgv,
     AnophelesKaryotypeAnalysis,
     AnophelesAimData,
